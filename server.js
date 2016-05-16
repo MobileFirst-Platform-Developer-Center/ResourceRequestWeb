b4aaf6d0934e843bbea59d18a56c20618f6bebf0
Idan Adar <idan@adar.me>
2016-05-16 14:04:11 +0300

    Update package.json and server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = 9081;
 
 server.listen(port);
 app.use('/myapp', express.static(__dirname + '/'));
-console.log('Listening on port ' + $port);
+console.log('::: server.js ::: Listening on port ' + port);
 
 // Web server - serves the web application
 app.get('/home', function(req, res) {
@@ -19,6 +19,6 @@ app.get('/home', function(req, res) {
 // Reverse proxy, pipes the requests to/from MobileFirst Server
 app.use('/mfp/*', function(req, res) {
     var url = 'http://localhost:9080' + req.originalUrl;
-    console.log('passing to URL: ' + url);
+    console.log('::: server.js ::: Passing request to URL: ' + url);
     req.pipe(request[req.method.toLowerCase()](url)).pipe(res);
 });
